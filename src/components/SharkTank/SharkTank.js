2b161a3e83ce1b7a590cde7919ae9904632298b2
John Thielman <john.thielman@gmail.com>
2019-12-16 20:02:21 -0600

    improves distribution of code among modules

--- src/components/SharkTank/SharkTank.js
+++ src/components/SharkTank/SharkTank.js
@@ -9,14 +9,26 @@ import LiveStudent from '../LiveStudent/LiveStudent';
 class SharkTank extends React.Component {
   static propTypes = {
     students: PropTypes.arrayOf(studentShape.studentShape),
+    attackAStudent: PropTypes.func,
   }
 
   render() {
     const myStudents = this.props.living;
+    const { attackAStudent } = this.props;
 
     const livingCards = myStudents.map((student) => (<LiveStudent key={student.id} student={student} />));
 
-    return (livingCards);
+    return (
+      <div className='container'>
+        <h1>Shark Tank</h1>
+        <div className='sharkTank border border-primary'>
+          <button className='btn btn-danger m-3' onClick={attackAStudent}>SHARK ATTACK</button>
+          <div className='row card-deck'>
+            {livingCards}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
